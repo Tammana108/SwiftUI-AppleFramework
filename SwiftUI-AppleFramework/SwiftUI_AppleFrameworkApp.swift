4e11bb7fba6ca14d3f1d4bd4bf5b106f694a8d40
Tammana Sharma <tammana.sharma@patientbond.com>
2023-10-05 14:54:04 +0530

    Initial Commit

---
+++ SwiftUI-AppleFramework/SwiftUI_AppleFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleFrameworkApp.swift
+//  SwiftUI-AppleFramework
+//
+//  Created by Tammana on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
